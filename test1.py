2695b18dcb67354858d225103a2c69bf5f0ef4ba
Yerong <yerong.li@outlook.com>
2021-06-09 15:35:04 -0500

    Update evaluation

--- test1.py
+++ test1.py
@@ -19,3 +19,4 @@ voca_emb_dir = "../data/generated/embeddings/word_ent_embs/"
 
 entity_voca, entity_embeddings = utils.load_voca_embs(voca_emb_dir + 'dict.entity',
                                                           voca_emb_dir + 'entity_embeddings.npy')
+print(type(entity_voca))
