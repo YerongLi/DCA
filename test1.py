218cc98298a88a1db6433af3502970e507d5a273
Yerong <yerong.li@outlook.com>
2021-06-09 15:32:36 -0500

    Update evaluation

--- test1.py
+++ test1.py
@@ -1,5 +1,6 @@
 import pickle
 import json
+import utils
 
 ent_inlinks_path = "../data/entityid_dictid_inlinks_uniq.pkl"
 
@@ -12,3 +13,6 @@ print(ent_inlinks_dict[262048])
 
 ent_desc = json.load(open('../data/ent2desc.json', 'r'))
 print(ent_desc['German_language'])
+
+entity_voca, entity_embeddings = utils.load_voca_embs(voca_emb_dir + 'dict.entity',
+                                                          voca_emb_dir + 'entity_embeddings.npy')
