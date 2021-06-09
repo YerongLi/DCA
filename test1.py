fb9788b33c8a3a780f9397087e3c5fabee1b4306
Yerong <yerong.li@outlook.com>
2021-06-09 16:15:11 -0500

    Update evaluation

--- test1.py
+++ test1.py
@@ -20,8 +20,9 @@ voca_emb_dir = "../data/generated/embeddings/word_ent_embs/"
 entity_voca, entity_embeddings = utils.load_voca_embs(voca_emb_dir + 'dict.entity',
                                                           voca_emb_dir + 'entity_embeddings.npy')
 														  
-i = entity_voca.word2id['en.wikipedia.org/wiki/Germany']
+i = entity_voca.word2id['en.wikipedia.org/wiki/German_language']
 print(i)
+print((entity_voca.id2word[i])
 for j in ent_inlinks_dict[i]:
 	print(entity_voca.id2word[j])
 print(ent_inlinks_dict[i])
