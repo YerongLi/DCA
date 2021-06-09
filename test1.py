ca55f909e15ec1ed5050248eb8dc70dc3938c3c7
Yerong <yerong.li@outlook.com>
2021-06-09 14:08:16 -0500

    Update evaluation

--- test1.py
+++ test1.py
@@ -7,4 +7,4 @@ ent_inlinks_path = "../data/entityid_dictid_inlinks_uniq.pkl"
 with open(ent_inlinks_path, 'rb') as f_pkl:
 	ent_inlinks_dict = pickle.load(f_pkl)
 
-print(ent_inlinks_dict.keys())
+print(ent_inlinks_dict[262048])
