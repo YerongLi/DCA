7f617c0405134c54404a144cb54188eedf2ef491
Yerong <yerong.li@outlook.com>
2021-06-17 17:38:44 -0500

    parse the training data

--- parse.py
+++ parse.py
@@ -8,6 +8,7 @@ with open('../data/generated/test_train_data/aida_train.csv') as f:
 		if docid not in tqdm.tqdm(dicmention):
 			dicmention[docid] = dict()
 		if mention not in dicmention[docid]:
-			dicmention[docid][mention] = (l[4:-2], l[-1])
-		assert l[4:-2] ==  dicmention[docid][mention][0]
+			dicmention[docid][mention] = (l[6:-2], l[-1])
+		if not l[6:-2] ==  dicmention[docid][mention][0]:
+			raise AssertionError
 	print(dicmention['1 EU)']['German'])
