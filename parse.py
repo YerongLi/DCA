4138bbcbb64f1026ff008fde5d117380fc51f1f9
Yerong <yerong.li@outlook.com>
2021-06-17 15:42:41 -0500

    parse the training data

--- parse.py
+++ parse.py
@@ -1,4 +1,10 @@
 with open('../data/generated/test_train_data/aida_train.csv') as f:
+	docset = set()
 	for line in f:
-		print(line.split('\t'))
+		l = line.split('\t')
+		docset.add(l[0])
+		if l[1] in docset:
+			continue
+		if l[2] == 'German':
+			print(l)
 		break
