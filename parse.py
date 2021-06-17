bb11aae54943689a181135346cbb690ab4e3db64
Yerong <yerong.li@outlook.com>
2021-06-17 17:35:58 -0500

    parse the training data

--- parse.py
+++ parse.py
@@ -1,9 +1,12 @@
 with open('../data/generated/test_train_data/aida_train.csv') as f:
-	docset = set()
+	dicmention = dict()
 	for line in f:
 		l = line.split('\t')
-		if l[1] in docset:
-			continue
-		docset.add(l[0])
-		if l[2] == 'German':
-			print(l)
+		docid = l[1]
+		mention = l[2]
+		if docid not in dicmention:
+			dicmention[docid] = dict()
+		if mention not in dicmention[docid]:
+			dicmention[docid][mention] = (l[5:-2], l[-1])
+		assert l[5:-2] ==  dicmention[docid][mention][0]
+	print(dicmention['1 EU)']['German'])
