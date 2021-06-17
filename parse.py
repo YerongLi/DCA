a5fb41ea12245c6f94188404c9af25f200f3466f
Yerong <yerong.li@outlook.com>
2021-06-17 15:34:24 -0500

    parse the training data

--- parse.py
+++ parse.py
@@ -1,3 +1,6 @@
 with open('../data/generated/test_train_data/aida_train.csv') as f:
-	line = f.readline()
-	print(line)
+	for line in f:
+		print(line)
+		break
+	# line = f.readline()
+	# print(line)
