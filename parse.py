8da33a350689add5662c9f7e3c5154487c229bbe
Yerong <yerong.li@outlook.com>
2021-06-17 17:42:42 -0500

    parse the training data

--- parse.py
+++ parse.py
@@ -1,11 +1,11 @@
 import tqdm
 with open('../data/generated/test_train_data/aida_train.csv') as f:
 	dicmention = dict()
-	for line in f:
+	for line in tqdm.tqdm((f):
 		l = line.split('\t')
 		docid = l[1]
 		mention = l[2]
-		if docid not in tqdm.tqdm(dicmention):
+		if docid not in dicmention:
 			dicmention[docid] = dict()
 		if mention not in dicmention[docid]:
 			dicmention[docid][mention] = (l[6:-2], l[-1])
