1003aa2c4dda06c5291921aae615fb816ae87785
Yerong <yerong.li@outlook.com>
2021-06-18 18:16:59 -0500

    generate dataset

--- parse.py
+++ parse.py
@@ -1,4 +1,10 @@
 import tqdm
+import json
+ent_desc = json.load(open('../data/ent2desc.json', 'r'))
+voca_emb_dir = "../data/generated/embeddings/word_ent_embs/"
+
+entity_voca, entity_embeddings = utils.load_voca_embs(voca_emb_dir + 'dict.entity',
+                                                          voca_emb_dir + 'entity_embeddings.npy')
 with open('../data/generated/test_train_data/aida_train.csv') as f:
 	dicmention = dict()
 	for line in tqdm.tqdm(f):
@@ -10,13 +16,15 @@ with open('../data/generated/test_train_data/aida_train.csv') as f:
 		if mention not in dicmention[docid]:
 			dicmention[docid][mention] = (l[6:-2], l[-1][:-1])
 		(candidates, groundtruth) = dicmention[docid][mention]
-		candidates = [c[c.index(',', c.index(',') + 1) + 1:] for c in candidates]
+		candidates = [c[c.index(',', c.index(',') + 1) + 1:].replace(' ', '_') for c in candidates]
 		assert(l[6:-2] == dicmention[docid][mention][0])
+		if not all([c in entity_voca.word2id for c in candidates]):
+			print(candidates)
 		# if not l[6:-2] ==  dicmention[docid][mention][0]:
 		# 	# print(l[6:-2])
 		# 	# print(dicmention[docid][mention][0])
 		# 	# raise AssertionError
 		# 	break
-	(candidates, groundtruth) =dicmention['44 SOCCER)']['Sion']
-	candidates = [c[c.index(',', c.index(',') + 1) + 1:] for c in candidates]
-	print(candidates)
+	# (candidates, groundtruth) =dicmention['44 SOCCER)']['Sion']
+	# candidates = [c[c.index(',', c.index(',') + 1) + 1:] for c in candidates]
+	# print(candidates)
