cc7f3ccbcd1da6b8eb5806c862777d99030178bf
Yerong <yerong.li@outlook.com>
2021-06-18 17:54:23 -0500

    generate dataset

--- parse.py
+++ parse.py
@@ -9,7 +9,9 @@ with open('../data/generated/test_train_data/aida_train.csv') as f:
 			dicmention[docid] = dict()
 		if mention not in dicmention[docid]:
 			dicmention[docid][mention] = (l[6:-2], l[-1][:-1])
-		assert(l[6:-2] == dicmention[docid][mention][0])
+		(candidates, groundtruth) = dicmention[docid][mention]
+		candidates = [c.split(',')[-1] for c in candidates]
+		# assert(l[6:-2] == dicmention[docid][mention][0])
 		# if not l[6:-2] ==  dicmention[docid][mention][0]:
 		# 	# print(l[6:-2])
 		# 	# print(dicmention[docid][mention][0])
