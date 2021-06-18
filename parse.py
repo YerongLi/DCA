a63143600fcc6808af9d74e127c59adb69490bb4
Yerong <yerong.li@outlook.com>
2021-06-18 18:21:23 -0500

    generate dataset

--- parse.py
+++ parse.py
@@ -8,6 +8,7 @@ entity_voca, entity_embeddings = utils.load_voca_embs(voca_emb_dir + 'dict.entit
                                                           voca_emb_dir + 'entity_embeddings.npy')
 with open('../data/generated/test_train_data/aida_train.csv') as f:
 	dicmention = dict()
+	count  = 0
 	for line in tqdm.tqdm(f):
 		l = line.split('\t')
 		docid = l[1]
@@ -19,8 +20,10 @@ with open('../data/generated/test_train_data/aida_train.csv') as f:
 		(candidates, groundtruth) = dicmention[docid][mention]
 		candidates = [c[c.index(',', c.index(',') + 1) + 1:].replace(' ', '_') for c in candidates]
 		assert(l[6:-2] == dicmention[docid][mention][0])
+
 		if not all(['en.wikipedia.org/wiki/' + c in entity_voca.word2id for c in candidates]):
-			print(candidates)
+			count+= 1
+			print(candidates, count)
 		# if not l[6:-2] ==  dicmention[docid][mention][0]:
 		# 	# print(l[6:-2])
 		# 	# print(dicmention[docid][mention][0])
