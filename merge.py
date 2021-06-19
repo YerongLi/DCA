28bcdc5a9b562b39dae5ee50f05bf5b904b8706d
Yerong <yerong.li@outlook.com>
2021-06-18 22:51:28 -0500

    generate dataset

--- merge.py
+++ merge.py
@@ -9,8 +9,8 @@ voca_emb_dir = "../data/generated/embeddings/word_ent_embs/"
 
 entity_voca, entity_embeddings = utils.load_voca_embs(voca_emb_dir + 'dict.entity',
                                                           voca_emb_dir + 'entity_embeddings.npy')
-
-directdict = {entity.split('/')[-1] : ent_desc [entity_voca.word2id[entity]] for entity in entity_voca.word2id \
+print(ent_desc.keys())
+directdict = {entity.split('/')[-1] : ent_desc[entity_voca.word2id[entity]] for entity in entity_voca.word2id \
 		if entity_voca.word2id[entity] in ent_desc}
 print(json.dumps(directdict, indent=2))
 	
