4cf17250fbba0949ee24cdc9d4869719cd87c3bf
Yerong <yerong.li@outlook.com>
2021-06-19 03:18:22 -0500

    generate dataset

--- dataset.py
+++ dataset.py
@@ -312,7 +312,6 @@ class CoNLLDatasetOnly:
         self.train = read_csv_file(path + '/aida_train.csv')
         self.testA = read_csv_file(path + '/aida_testA.csv')
         self.testB = read_csv_file(path + '/aida_testB.csv')
-        print(self.train.keys())
         # self.msnbc = read_csv_file(path + '/wned-msnbc.csv')
         # self.ace2004 = read_csv_file(path + '/wned-ace2004.csv')
         # self.aquaint = read_csv_file(path + '/wned-aquaint.csv')
