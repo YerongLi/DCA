69f281b188792337fa276e589e9dc01cf5d161e7
Yerong <yerong.li@outlook.com>
2021-06-19 00:34:27 -0500

    generate dataset

--- main.py
+++ main.py
@@ -166,6 +166,7 @@ if __name__ == "__main__":
     print('load conll at', datadir)
     conll = D.CoNLLDataset(datadir, conll_path, person_path, args.order, args.method)
     print(type(conll.train))
+    print(conll.train.keys())
     sys.exit()
     print('create model')
     word_voca, word_embeddings = utils.load_voca_embs(voca_emb_dir + 'dict.word',
