15ddd7fedcda9c9f0a274a85515f5cf2a50134df
Yerong <yerong.li@outlook.com>
2021-06-21 14:23:15 -0500

    dump reference count and type

--- dump_ref.py
+++ dump_ref.py
@@ -29,7 +29,7 @@ conll = D.CoNLLDatasetOnly(datadir, conll_path, person_path, 'offset', 'SL')
 # print(conll.train['528 SQUASH) 528 SQUASH)'][1]['context'])
 # conll.train['528 SQUASH) 528 SQUASH)'][0].keys()
 datasets = [('train', conll.train), ('testA', conll.testA), ('testB', conll.testB)]
-resjon = dict{}
+resjon = dict()
 def dump(dataset):
 	(pos, dataset) = dataset
 	(name , dictionary) = dataset
