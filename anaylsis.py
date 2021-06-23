1b2109c19e6a426cdc0ef4c183bb5a46b9133900
Yerong <yerong.li@outlook.com>
2021-06-23 15:14:52 -0500

    test inlinks

--- anaylsis.py
+++ anaylsis.py
@@ -5,25 +5,25 @@ df_ = pd.read_csv('full_testA.csv')
 
 n, l, r = df_.shape[0], 0, 0
 
-try:
-	while r < n:
-		while r < n - 1 and df_.iloc[l]['QuestionMention'] == df_.iloc[r + 1]['QuestionMention']:
-			r+= 1
-		batch = df_.iloc[l : r + 1]
-		gt = batch[batch.Label.eq(1)].values
-		gtn = ['Mention_label'].split(';')[1]
-		print(gtn)
-		# # assert(len(gold_pairs) == 1)
-		# gt = gt[0].split(';')[1].replace(' ', '_')
-		# # j = torch.argmax(pred_[l:r + 1]).numpy()
-		# # print(j)
-		# if j != r - l:
-		# 	data.append([df_.iloc[l]['QuestionMention'],
-		# 	f'https://dbpedia.org/page/{gt}', 
-		# 	f"https://dbpedia.org/page{batch.iloc[j]['Mention_label'].split(';')[1].replace(' ', '_')}"])
-		l = r + 1
-		r = l
-	error_df =  pd.DataFrame(data, columns = ['QuestionMention','gold','pred'])
-	error_df.to_csv('error.csv', index=False)
-except:
-	pass
+# try:
+while r < n:
+	while r < n - 1 and df_.iloc[l]['QuestionMention'] == df_.iloc[r + 1]['QuestionMention']:
+		r+= 1
+	batch = df_.iloc[l : r + 1]
+	gt = batch[batch.Label.eq(1)].values
+	gtn = ['Mention_label'].split(';')[1]
+	print(gtn)
+	# # assert(len(gold_pairs) == 1)
+	# gt = gt[0].split(';')[1].replace(' ', '_')
+	# # j = torch.argmax(pred_[l:r + 1]).numpy()
+	# # print(j)
+	# if j != r - l:
+	# 	data.append([df_.iloc[l]['QuestionMention'],
+	# 	f'https://dbpedia.org/page/{gt}', 
+	# 	f"https://dbpedia.org/page{batch.iloc[j]['Mention_label'].split(';')[1].replace(' ', '_')}"])
+	l = r + 1
+	r = l
+# 	error_df =  pd.DataFrame(data, columns = ['QuestionMention','gold','pred'])
+# 	error_df.to_csv('error.csv', index=False)
+# except:
+	# pass
