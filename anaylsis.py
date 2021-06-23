647aacc1e57b2a1d21ba652d18abc76c062c08d3
Yerong <yerong.li@outlook.com>
2021-06-23 15:25:48 -0500

    test inlinks

--- anaylsis.py
+++ anaylsis.py
@@ -9,16 +9,19 @@ n, l, r = df_.shape[0], 0, 0
 while r < n:
 	while r < n - 1 and df_.iloc[l]['QuestionMention'] == df_.iloc[r + 1]['QuestionMention']:
 		r+= 1
-	batch = df_.iloc[l : r + 1]
-	gt = batch[batch.Label.eq(1)]
-	# print(gt)
-	gtn = gt.Mention_label.values[0].split(';')[1]
-	features = batch.Features.values
-	features = [eval(f)[16] for f in features]
-	m = max(features)
-	mi = features.index(m)
-	pred = batch.Mention_label.values[mi].split(';')[1]
-	print(gt.QuuestionMention.values[0], gt, pred)
+	try:
+		batch = df_.iloc[l : r + 1]
+		gt = batch[batch.Label.eq(1)]
+		# print(gt)
+		gtn = gt.Mention_label.values[0].split(';')[1]
+		features = batch.Features.values
+		features = [eval(f)[16] for f in features]
+		m = max(features)
+		mi = features.index(m)
+		pred = batch.Mention_label.values[mi].split(';')[1]
+		print(gt.QuuestionMention.values[0], gt, pred)
+	except:
+		pass
 
 	# # assert(len(gold_pairs) == 1)
 	# gt = gt[0].split(';')[1].replace(' ', '_')
