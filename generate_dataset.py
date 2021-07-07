6b3f1c12f42185e16bce16641652b40209d6f673
Yerong <yerong.li@outlook.com>
2021-07-07 06:09:01 -0500

    generate dataaset

--- generate_dataset.py
+++ generate_dataset.py
@@ -54,8 +54,7 @@ def generate_csv(dataset):
 				# print(entry["context"])
 				# print(mention)
 				# sys.exit()
-				data.append([entry["context"][0],
-					f'{mention}==={cname}',
+				data.append([entry["context"][0], f'{mention}==={cname}',
 					str(featurev),
 					1 if c == groundtruth else 0,
 					mention,
