0d4581175a2925fb7b9a3b4c79ea330786541c43
Yerong <yerong.li@outlook.com>
2021-07-07 02:01:07 -0500

    scores

--- ed_ranker.py
+++ ed_ranker.py
@@ -1158,6 +1158,7 @@ class EDRanker:
                     predictions[dname].append({'pred': (entity, 0.)})
                     predictions_score[dname].append({'pred': (entity, 0.)})
             #self.record.append(dict({'added_words':self.added_words, 'added_ents':self.added_ents}))
-        print('predictions_score', predictions_score.keys())
+        if '1094testa 1094testa' in predictions_score:
+            print('predictions_score', predictions_score['1094testa 1094testa'])
         return predictions, predictions_score
 
