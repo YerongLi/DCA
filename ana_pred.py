938aabac3e9787175f9697bed77c5ba437a3a0e9
Yerong <yerong.li@outlook.com>
2021-07-07 07:46:28 -0500

    dataset

--- ana_pred.py
+++ ana_pred.py
@@ -1,12 +1,14 @@
 import json
 import itertools
-predicions_score = json.load(open('aida-train.json', 'r'))
-def flatten(listOfLists):
-    "Flatten one level of nesting"
-    return itertools.chain.from_iterable(listOfLists)
-scores = [[list(pred['pred'].values()) for pred in predicions_score[doc_]] for doc_ in predicions_score]
-scores = list(flatten(scores))
-scores = list(flatten(scores))
-scores = [float(s) for s in scores if s != '-100']
-print(max(scores))
-print(min(scores))
+datasets = ['aida-train.json', 'aida-A.json', 'aida-B.json']
+for dataset in datasets:
+	predicions_score = json.load(open(dataset, 'r'))
+	def flatten(listOfLists):
+		"Flatten one level of nesting"
+		return itertools.chain.from_iterable(listOfLists)
+	scores = [[list(pred['pred'].values()) for pred in predicions_score[doc_]] for doc_ in predicions_score]
+	scores = list(flatten(scores))
+	scores = list(flatten(scores))
+	scores = [float(s) for s in scores if s != '-100']
+	print(max(scores))
+	print(min(scores))
