a84e817b393e59782c241fe9ad2b4ca9f61b893b
Yerong <yerong.li@outlook.com>
2021-07-07 02:10:55 -0500

    scores

--- ed_ranker.py
+++ ed_ranker.py
@@ -1117,7 +1117,7 @@ class EDRanker:
                                                  isOrderFixed=True, isSort=self.args.sort)
 
             scores = scores.cpu().data.numpy()
-            print('scores', len(scores), len(scores[0]))
+            # print('scores', len(scores), len(scores[0]))
             pred_ids = np.argmax(scores, axis=1)
             end_time = time.time()
             if self.rt_flag:
