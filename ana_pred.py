171ea47fc129a5c841b2c330c5f1e11e3f033836
Yerong <yerong.li@outlook.com>
2021-07-07 07:41:18 -0500

    dataset

--- ana_pred.py
+++ ana_pred.py
@@ -1,5 +1,10 @@
 import json
+import itertools
 predicions_score = json.load(open('aida-train.json', 'r'))
-
+def flatten(listOfLists):
+    "Flatten one level of nesting"
+    return itertools.chain.from_iterable(listOfLists)
 scores = [[list(pred['pred'].values()) for pred in predicions_score[doc_]] for doc_ in predicions_score]
+scores = flatten(scores)
+scores = flatten(scores)
 print(scores)
