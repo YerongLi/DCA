542a49deebccfbf797b5b22d90e17519862aeb9d
Yerong <yerong.li@outlook.com>
2021-07-07 07:37:54 -0500

    dataset

--- ana_pred.py
+++ ana_pred.py
@@ -1,5 +1,5 @@
 import json
 predicions_score = json.load(open('aida-train.json', 'r'))
 
-scores = [[pred['pred'] for pred in predicions_score[doc_]] for doc_ in predicions_score]
+scores = [[pred['pred'].values() for pred in predicions_score[doc_]] for doc_ in predicions_score]
 print(scores)
