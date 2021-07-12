5815c09f6e4c1b4bc24c7ea5e193fa61f0b0f677
Yerong <yerong.li@outlook.com>
2021-07-12 01:44:40 -0500

    generate dataset

--- check.py
+++ check.py
@@ -11,6 +11,7 @@ def check(filename):
 			r+= 1
 		batch = df.iloc[l : r + 1]
 		ground_truth = batch[batch.Label.eq(1)]
-		assert(ground_truth.shape[0] == 1)
+		if ground_truth.shape[0] != 1:
+			print(ground_truth)
 with multiprocessing.Pool(3) as p:
 	p.map(check, datasets)
