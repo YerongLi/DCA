db85f9bd367b0f32db90e80b925e917dab9771e7
Yerong <yerong.li@outlook.com>
2021-07-13 11:38:25 -0500

    insert text

--- inserttext.py
+++ inserttext.py
@@ -4,4 +4,7 @@ client = pymongo.MongoClient(host='localhost', port=27017)
 db = client.dbpedia
 
 document = db.document
-json.load(open('d.json','r'))
+js = json.load(open('d.json','r'))
+for entry in js:
+	if document.find_one({'-id', entry['_id']}):
+		document.update_one({'-id', entry['_id']}, {'$set' : {'text' : entry['text']}})
