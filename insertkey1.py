41e2be79ae3b00bcad945b8d531d21a82a02f0a2
Yerong <yerong.li@outlook.com>
2021-07-13 12:09:58 -0500

    insert text

--- insertkey1.py
+++ insertkey1.py
@@ -1,6 +1,20 @@
 import pymongo
 import json
+import spacy
+import pytextrank
 client = pymongo.MongoClient(host='localhost', port=27017)
 db = client.dbpedia
+nlp = spacy.load("en_core_web_lg")
 
+# add PyTextRank to the spaCy pipeline
+nlp.add_pipe("textrank")
 document = db.document
+
+for entry in document.find():
+		# title = entry['title'] if 'title' in entry else ''
+		doc = nlp(entry['text'])
+		for phrase in doc._.phrases:
+		print(phrase.text)
+		print(phrase.rank, phrase.count)
+		print(phrase.chunks)
+		# document.update_one({'_id': entry['_id']}, {'$set' : {'key1' : entry['text']}})
